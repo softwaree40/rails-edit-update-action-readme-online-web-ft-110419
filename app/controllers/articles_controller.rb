4644ccca24651b88979e0da1d13e9d2da1cc81c1
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-01 21:19:20 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
    
    def update 
      @article = Article.find(params[:id])
-     @article.update(title: params[:article][:title], description: params[:article][:description])
+     @article.update(title: params[:title], description: params[:description])
      redirect_to article_path(@article)
    end
   # add edit and update methods here
