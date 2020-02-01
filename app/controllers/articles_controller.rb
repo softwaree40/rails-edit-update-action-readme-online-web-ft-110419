4e0f989da1af29391a8707b91d737842e28499f1
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-01 21:18:59 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,9 +18,11 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+  
    def edit 
       @article = Article.find(params[:id])
    end
+   
    def update 
      @article = Article.find(params[:id])
      @article.update(title: params[:article][:title], description: params[:article][:description])
